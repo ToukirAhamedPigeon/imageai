6987e71d7b7169ffc38ece404b42f09b8c209bd1
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-05-03 21:59:30 +0600

    Icon Logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <Image src="/logo.png" alt="logo" width={100} height={100} />
       <Button size="lg" className="bg-lime-500 text-white">Hello World</Button>
     </div>
   );
