fb1bbc63f3bec53137f527042df297e7f48aeca1
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-05-09 20:45:56 +0600

    all

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "Modify images with AI",
     icons: [
         {
-            src: "/logo-icon.png",
+            src: "/icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/logo.png",
+            src: "/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png"
           }
